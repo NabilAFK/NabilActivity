7045955ed621130214eb1fc68e8c3f528dec82f2
Nabil <81329611+NabilAFK@users.noreply.github.com>
2021-03-24 16:32:09 -0700

    Update background.js

--- Content/background.js
+++ Content/background.js
@@ -13,3 +13,145 @@ const resetActivity=()=>{
 		})
 	}
 }
+chrome.storage.local.get(["source"],result=>source=result.source)
+chrome.runtime.onConnect.addListener(port=>{
+	if(port.name=="discord")
+	{
+		if(discordPort!==undefined)
+		{
+			discordPort.postMessage({action:"close"})
+			discordPort.disconnect()
+		}
+		discordPort=port
+		console.info("Discord port opened")
+		port.onDisconnect.addListener(()=>{
+			console.info("Discord port closed")
+			discordPort=undefined
+			if(source=="youtube"&&youtubePort!==undefined)
+			{
+				youtubePort.postMessage({listen:false});
+			}
+			if(source=="youtubemusic"&&youtubeMusicPort!==undefined)
+			{				
+				youtubeMusicPort.postMessage({listen:false});
+			}
+		})
+		if(source=="off")
+		{
+			resetActivity()
+		}
+		else if(source=="custom")
+		{
+			chrome.storage.local.get(["type","name","streamurl","details","state","partycur","partymax"],result=>discordPort.postMessage(result))
+		}
+		else if(source=="youtube"&&youtubePort!==undefined)
+		{
+			youtubePort.postMessage({listen:true})
+		}
+		else if(source=="youtubemusic"&&youtubeMusicPort!==undefined)
+		{
+			youtubeMusicPort.postMessage({listen:true})
+		}
+		else if(source=="soundcloud"&&soundcloudPort!==undefined)
+		{
+			soundcloudPort.postMessage({listen:true})
+		}
+		else
+		{
+			resetActivity()
+		}
+	}
+	else if(port.name=="youtube")
+	{
+		if(youtubePort!==undefined)
+		{
+			youtubePort.postMessage({action:"close"})
+			youtubePort.disconnect()
+		}
+		youtubePort=port
+		console.info("YouTube port opened")
+		port.onDisconnect.addListener(()=>{
+			console.info("YouTube port closed")
+			youtubePort=undefined
+			if(source=="youtube")
+			{
+				resetActivity()
+			}
+		})
+		if(source=="youtube"&&discordPort!==undefined)
+		{
+			youtubePort.postMessage({listen:true})
+		}
+	}
+	else if(port.name=="youtubemusic")
+	{
+		if(youtubeMusicPort!==undefined)
+		{
+			youtubeMusicPort.postMessage({action:"close"})
+			youtubeMusicPort.disconnect()
+		}
+		youtubeMusicPort=port
+		console.info("YouTube Music port opened")
+		port.onDisconnect.addListener(()=>{
+			console.info("YouTube Music port closed")
+			youtubeMusicPort=undefined
+			if(source=="youtubemusic")
+			{
+				resetActivity()
+			}
+		})
+		if(source=="youtubemusic"&&discordPort!==undefined)
+		{
+			youtubeMusicPort.postMessage({listen:true})
+		}
+	}
+	else if(port.name=="soundcloud")
+	{
+		if(soundcloudPort!==undefined)
+		{
+			soundcloudPort.postMessage({action:"close"})
+			soundcloudPort.disconnect()
+		}
+		soundcloudPort=port
+		console.info("SoundCloud port opened")
+		port.onDisconnect.addListener(()=>{
+			console.info("SoundCloud port closed")
+			soundcloudPort=undefined
+			if(source=="soundcloud")
+			{
+				resetActivity()
+			}
+		})
+		if(source=="soundcloud"&&discordPort!==undefined)
+		{
+			soundcloudPort.postMessage({listen:true})
+		}
+	}
+	else if(port.name=="plex")
+	{
+		if(plexPort!==undefined)
+		{
+			plexPort.postMessage({action:"close"})
+			plexPort.disconnect()
+		}
+		plexPort=port
+		console.info("Plex port opened")
+		port.onDisconnect.addListener(()=>{
+			console.info("Plex port closed")
+			plexPort=undefined
+			if(source=="plex")
+			{
+				resetActivity()
+			}
+		})
+		if(source=="plex"&&discordPort!==undefined)
+		{
+			plexPort.postMessage({listen:true})
+		}
+	}
+	else
+	{
+		console.error("Denied connection with unexpected name:",port.name)
+		port.disconnect()
+	}
+}) 
