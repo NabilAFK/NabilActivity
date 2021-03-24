646cc7e99ccbe42cfcbb74903b6826bb49875468
Nabil <81329611+NabilAFK@users.noreply.github.com>
2021-03-24 15:39:58 -0700

    Update popup.js

--- Popup/popup.js
+++ Popup/popup.js
@@ -16,3 +16,47 @@ for(let i in elms)
 		elm.setAttribute("placeholder",chrome.i18n.getMessage(elm.getAttribute("data-placeholder")))
 	}
 }
+
+document.getElementById("source").onchange=()=>{
+	let source=document.getElementById("source").value
+	if(source=="custom")
+	{
+		document.getElementById("content-source-custom").className=""
+	}
+	else
+	{
+		document.getElementById("content-source-custom").className="hidden"
+	}
+	if(source=="youtube")
+	{
+		document.getElementById("content-source-youtube").className=""
+	}
+	else
+	{
+		document.getElementById("content-source-youtube").className="hidden"
+	}
+	if(source=="youtubemusic")
+	{
+		document.getElementById("content-source-youtube-music").className=""
+	}
+	else
+	{
+		document.getElementById("content-source-youtube-music").className="hidden"
+	}
+	if(source=="soundcloud")
+	{
+		document.getElementById("content-source-soundcloud").className=""
+	}
+	else
+	{
+		document.getElementById("content-source-soundcloud").className="hidden"
+	}
+	if(source=="plex")
+	{
+		document.getElementById("content-source-plex").className=""
+	}
+	else
+	{
+		document.getElementById("content-source-plex").className="hidden"
+	}
+}
