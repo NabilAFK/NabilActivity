2c7d7388c9043d6fab551a70c695d8c1981d6042
Nabil <81329611+NabilAFK@users.noreply.github.com>
2021-03-24 13:50:40 -0700

    Update discord.js

--- discord.js
+++ discord.js
@@ -61,3 +61,92 @@ const injectionCode=()=>{
 			this.downstreamSocket.send(d)
 		}
 	}
+        window.WebSocket.prototype.close=function(c,r)
+	{
+		this.downstreamSocket.close(c,r)
+	}
+	window.WebSocket.CONNECTING=originalWebSocket.CONNECTING
+	window.WebSocket.OPEN=originalWebSocket.OPEN
+	window.WebSocket.CLOSING=originalWebSocket.CLOSING
+	window.WebSocket.CLOSED=originalWebSocket.CLOSED
+	window.SetDiscordActivitySendStatus=()=>{
+		if(window.SetDiscordActivityActiveSocket&&window.SetDiscordActivityActiveSocket.readyState==originalWebSocket.OPEN)
+		{
+			let activity={
+				type:window.SetDiscordActivityData.activityType,
+				name:window.SetDiscordActivityData.activityName
+			}
+			if(window.SetDiscordActivityData.activityType==1)
+			{
+				activity.url=window.activityUrl
+			}
+			if(window.SetDiscordActivityData.activityPartyCur!=""&&window.SetDiscordActivityData.activityPartyMax!="")
+			{
+				activity.party={size:[window.SetDiscordActivityData.activityPartyCur,window.SetDiscordActivityData.activityPartyMax]}
+			}
+			if(window.SetDiscordActivityData.activityDetails)
+			{
+				activity.details=window.SetDiscordActivityData.activityDetails
+			}
+			if(window.SetDiscordActivityData.activityState)
+			{
+				activity.state=window.SetDiscordActivityData.activityState
+			}
+			window.SetDiscordActivityActiveSocket.send(JSON.stringify({op:3,d:{
+				status,
+				activities:[activity],
+				since,
+				afk
+			}}))
+		}
+	}
+},
+injectScript=text=>{
+	let script=document.createElement("script")
+	script.innerHTML=text
+	script=document.documentElement.appendChild(script)
+	setTimeout(()=>{
+		document.documentElement.removeChild(script)
+	},10)
+},
+encodeString=str=>str?str.split("\\").join("\\\\").split("\"").join("\\\""):str
+injectScript("("+injectionCode.toString()+")()")
+let port=chrome.runtime.connect({name:"discord"}),closeOK=false
+port.onMessage.addListener(msg=>{
+	console.info(msg)
+	if(msg.action)
+	{
+		switch(msg.action)
+		{
+			case"close":
+			closeOK=true
+			break;
+
+			default:
+			console.warn("Unknown action",msg.action)
+		}
+	}
+	else if(msg.type!==undefined&&msg.name!==undefined)
+	{
+		injectScript(`window.SetDiscordActivityData={
+			sendUpdate:true,
+			activityType:`+msg.type+`,
+			activityName:\"`+encodeString(msg.name)+`\",
+			activityUrl:\"`+encodeString(msg.streamurl)+`\",
+			activityDetails:\"`+encodeString(msg.details)+`\",
+			activityState:\"`+encodeString(msg.state)+`\",
+			activityPartyCur:\"`+encodeString(msg.partycur)+`\",
+			activityPartyMax:\"`+encodeString(msg.partymax)+`\"
+		}`)
+	}
+})
+port.onDisconnect.addListener(()=>{
+	console.info("port closed")
+	if(closeOK)
+	{
+		closeOK=false
+	}
+	else
+	{
+		location.reload()
+	}
