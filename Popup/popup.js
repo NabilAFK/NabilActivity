d974addbb4379ee925085a5891ebf3c84ea670e3
Nabil <81329611+NabilAFK@users.noreply.github.com>
2021-03-24 15:20:56 -0700

    Create popup.js

---
+++ Popup/popup.js
@@ -0,0 +1,17 @@
+let elms=document.querySelectorAll("[data-message]")
+for(let i in elms)
+{
+	let elm=elms[i]
+	if(elm instanceof HTMLElement)
+	{
+		elm.textContent=chrome.i18n.getMessage(elm.getAttribute("data-message"))
+	}
+}
+elms=document.querySelectorAll("[data-placeholder]")
+for(let i in elms)
+{
+	let elm=elms[i]
+	if(elm instanceof HTMLElement)
+	{
+		elm.setAttribute("placeholder",chrome.i18n.getMessage(elm.getAttribute("data-placeholder")))
+	}
